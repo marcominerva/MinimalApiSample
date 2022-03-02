fc74173aae9df9b582d60d9d0d8df3249a666fd1
Marco Minerva <marco.minerva@gmail.com>
2022-03-02 18:39:25 +0100

    Add Entity Framework DbContext

---
+++ MinimalApiSample/DataAccessLayer/Configurations/PersonConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MinimalApiSample.DataAccessLayer.Entities;
+
+namespace MinimalApiSample.DataAccessLayer.Configurations;
+
+public class PersonConfiguration : IEntityTypeConfiguration<Person>
+{
+    public void Configure(EntityTypeBuilder<Person> builder)
+    {
+        builder.ToTable("People");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+        builder.Property(x => x.FirstName).HasMaxLength(30).IsRequired();
+        builder.Property(x => x.LastName).HasMaxLength(30).IsRequired();
+        builder.Property(x => x.City).HasMaxLength(50);
+    }
+}
---
+++ MinimalApiSample/DataAccessLayer/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace MinimalApiSample.DataAccessLayer;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<string>().AreUnicode(false);
+
+        base.ConfigureConventions(configurationBuilder);
+    }
+}
