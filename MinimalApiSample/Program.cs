6307ac7bd2218989c87404ac6afc65b48f630e9e
Marco Minerva <marco.minerva@gmail.com>
2023-01-08 16:47:51 +0100

    Add ProblemDetails

--- MinimalApiSample/Program.cs
+++ MinimalApiSample/Program.cs
@@ -1,6 +1,10 @@
+using System.Diagnostics;
 using System.Net.Mime;
+using System.Text.Json.Serialization;
 using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.EntityFrameworkCore;
 using MinimalApiSample.DataAccessLayer;
 using MinimalApiSample.Extensions;
@@ -11,6 +15,11 @@ using Entities = MinimalApiSample.DataAccessLayer.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
+
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,10 +28,55 @@ builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 builder.Services.AddSqlServer<DataContext>(builder.Configuration.GetConnectionString("SqlConnection"));
 
+builder.Services.AddProblemDetails();
+
 var app = builder.Build();
 await ConfigureDatabaseAsync(app.Services);
 
 app.UseHttpsRedirection();
+
+if (!app.Environment.IsDevelopment())
+{
+    // Error handling
+    app.UseExceptionHandler(new ExceptionHandlerOptions
+    {
+        AllowStatusCode404Response = true,
+        ExceptionHandler = async (HttpContext context) =>
+        {
+            var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+            var error = exceptionHandlerFeature?.Error;
+
+            if (context.RequestServices.GetRequiredService<IProblemDetailsService>() is { } problemDetailsService)
+            {
+                // Write as JSON problem details
+                await problemDetailsService.WriteAsync(new()
+                {
+                    HttpContext = context,
+                    AdditionalMetadata = exceptionHandlerFeature?.Endpoint?.Metadata,
+                    ProblemDetails =
+                    {
+                        Status = context.Response.StatusCode,
+                        Title = error?.GetType().FullName ?? "an error occurred while processing your request",
+                        Detail = error?.Message
+                    }
+                });
+            }
+            else
+            {
+                context.Response.ContentType = MediaTypeNames.Text.Plain;
+                var message = ReasonPhrases.GetReasonPhrase(context.Response.StatusCode) switch
+                {
+                    { Length: > 0 } reasonPhrase => reasonPhrase,
+                    _ => "An error occurred"
+                };
+                await context.Response.WriteAsync(message + "\r\n");
+                await context.Response.WriteAsync($"Request ID: {Activity.Current?.Id ?? context.TraceIdentifier}");
+            }
+        }
+    });
+}
+
+app.UseStatusCodePages();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
